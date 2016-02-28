09e5a664ca2216e621603e1515a163dfb901ae7e
Brad Reardon <brad.jay.reardon@gmail.com>
2016-02-28 15:23:49 -0500

    Updated to fix offline voting

--- src/main/java/com/github/bradreardon/votetokens/listeners/VoteListener.java
+++ src/main/java/com/github/bradreardon/votetokens/listeners/VoteListener.java
@@ -1,12 +1,17 @@
 package com.github.bradreardon.votetokens.listeners;
 
+import com.evilmidget38.UUIDFetcher;
 import com.github.bradreardon.votetokens.VoteTokens;
 import com.github.bradreardon.votetokens.database.Log;
 import com.github.bradreardon.votetokens.database.VotePlayer;
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 import org.bukkit.Bukkit;
+import static org.bukkit.Bukkit.getLogger;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -28,18 +33,35 @@ public class VoteListener implements Listener {
     @EventHandler(priority=EventPriority.NORMAL)
     public void onVoteEvent(VotifierEvent event) {
         Vote vote = event.getVote();
-        String uuid = Bukkit.getPlayer(vote.getUsername()).getUniqueId().toString();
+        UUIDFetcher fetcher = new UUIDFetcher(Arrays.asList(vote.getUsername()));
+        
+        Map<String, UUID> response = null;
+        try {
+            response = fetcher.call();
+        } catch (Exception e) {
+            getLogger().warning("Exception while running UUIDFetcher");
+            e.printStackTrace();
+        }
+        
+        String uuid = response.get(vote.getUsername()).toString();
         VotePlayer player = plugin.getPlayerTable().getPlayer(uuid);
 
         player.setTokens(player.getTokens() + 1);
         plugin.getPlayerTable().update(player);
+        
+        String service;
+        if (vote.getServiceName() != null) {
+            service = vote.getServiceName();
+        } else {
+            service = "";
+        }
         
         Log log = new Log();
         log.setLogDate(new Date());
         log.setLogType("vote");
         log.setPlayer(vote.getUsername());
         log.setUuid(uuid);
-        log.setLogText(vote.getServiceName());
+        log.setLogText(service);
         plugin.getLogTable().save(log);
         
         Player p = plugin.getServer().getPlayer(vote.getUsername());
